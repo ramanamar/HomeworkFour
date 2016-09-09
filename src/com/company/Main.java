65a131f77d49ce2bf08c46dfd982bad785e89d5f
Raman Kryvasheyeu <kryvasheyeu@gmail.com>
2016-09-09 18:26:34 +0300

    Starting to implement ArrayList

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,25 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	Employee[] emplArray = new Employee[5];
+        Employee e = new Employee();
+	    Employee[] emplArray = new Employee[5];
         emplArray[0] = new Employee("Ivanov Ivan Ivanovich", "Director", "ivanow@company.com", "+885553636", 50000, 52 );
         emplArray[1] = new Employee("Petrov Petr Petrovich", "Accountant", "petrov@company.com", "+868435795", 30000, 43 );
         emplArray[2] = new Employee("Sidorov Sidor Sidorovich", "Manager", "sidorov@company.com", "+897465139", 20000, 32 );
         emplArray[3] = new Employee("Vodkin Fedor Fedorovich", "Driver", "vodkin@company.com", "+897541236", 18000, 40 );
         emplArray[4] = new Employee("Stukov Yriy Nefedovich", "Engineer", "stukov@company.com", "+836536598", 25000, 38 );
 
+        ArrayList eArr = new ArrayList();
+        eArr.add(e);
+
         for (int i = 0; i < emplArray.length; i++) {
-            emplArray[i].printInfo();
+            if (e.getAge() > 40) {
+                emplArray[i].printInfo();
+            }
         }
     }
 }
@@ -22,7 +30,7 @@ class Employee {
     protected String email;
     protected String phoneNumber;
     protected int salary;
-    static int age;
+    protected int age;
 
     public Employee (String name, String post, String email, String phoneNumber, int salary, int age) {
         this.name = name;
@@ -31,6 +39,17 @@ class Employee {
         this.phoneNumber = phoneNumber;
         this.salary = salary;
         this.age = age;
+    }
+    public Employee() {
+
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
     }
 
     public void printInfo () {
