6af73db5a31cbb4ecbf634ca537920615dbb2ac3
Raman Kryvasheyeu <kryvasheyeu@gmail.com>
2016-09-12 13:50:30 +0300

    Final commit. The job is done.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,26 +5,22 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        Employee e = new Employee();
-	    Employee[] emplArray = new Employee[5];
-        emplArray[0] = new Employee("Ivanov Ivan Ivanovich", "Director", "ivanow@company.com", "+885553636", 50000, 52 );
-        emplArray[1] = new Employee("Petrov Petr Petrovich", "Accountant", "petrov@company.com", "+868435795", 30000, 43 );
-        emplArray[2] = new Employee("Sidorov Sidor Sidorovich", "Manager", "sidorov@company.com", "+897465139", 20000, 32 );
-        emplArray[3] = new Employee("Vodkin Fedor Fedorovich", "Driver", "vodkin@company.com", "+897541236", 18000, 40 );
-        emplArray[4] = new Employee("Stukov Yriy Nefedovich", "Engineer", "stukov@company.com", "+836536598", 25000, 38 );
+        ArrayList <Employee> eArr = new ArrayList<>();    //Создаем ArrayList и добавляем в него сотрудников
+        eArr.add(new Employee("Ivanov Ivan Ivanovich", "Director", "ivanow@company.com", "+885553636", 50000, 52 ));
+        eArr.add(new Employee("Petrov Petr Petrovich", "Accountant", "petrov@company.com", "+868435795", 30000, 43 ));
+        eArr.add(new Employee("Sidorov Sidor Sidorovich", "Manager", "sidorov@company.com", "+897465139", 20000, 32 ));
+        eArr.add(new Employee("Vodkin Fedor Fedorovich", "Driver", "vodkin@company.com", "+897541236", 18000, 40 ));
+        eArr.add(new Employee("Stukov Yriy Nefedovich", "Engineer", "stukov@company.com", "+836536598", 25000, 38 ));
 
-        ArrayList eArr = new ArrayList();
-        eArr.add(e);
-
-        for (int i = 0; i < emplArray.length; i++) {
-            if (e.getAge() > 40) {
-                emplArray[i].printInfo();
+        for (Employee e: eArr) {    //Для каждого элемента из массива eArr получаем возраст через геттер и
+            if (e.getAge() >= 40) { //сравниваем полученное значение с заданным
+                e.printInfo();      //При совпадении, выводим элемент в консоль
             }
         }
     }
 }
 
-class Employee {
+class Employee {    //Создаем класс сотрудников
     protected String name;
     protected String post;
     protected String email;
@@ -32,6 +28,7 @@ class Employee {
     protected int salary;
     protected int age;
 
+    //Конструктор класса с параметрами, который заменяет конструктор по-умолчанию
     public Employee (String name, String post, String email, String phoneNumber, int salary, int age) {
         this.name = name;
         this.post = post;
@@ -40,19 +37,12 @@ class Employee {
         this.salary = salary;
         this.age = age;
     }
-    public Employee() {
 
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public int getAge() {
+    public int getAge() {    //Геттер для получения возраста сотрудника. Сеттер нам здесь не нужен
         return age;
     }
 
-    public void printInfo () {
+    public void printInfo () {    //Метод вывода информации о сотруднике
         System.out.println("Name: "+name+"\n"+"Post: "+post+"\n"+"E-mail: "+email+"\n"+"Phone: "
         +phoneNumber+"\n"+"Salary: "+salary+"\n"+"Age: "+age+"\n");
     }
